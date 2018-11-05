a3ef7eefe87f6b2ac07bc7f3c6450b38eab879e7
chlsmile <chl_smile@163.com>
2018-11-05 21:19:02 +0800

    add init

---
+++ src/main/java/com/chlsmile/log/App.java
@@ -0,0 +1,18 @@
+package com.chlsmile.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    final static Logger logger = LoggerFactory.getLogger("demo");
+
+    public static void main( String[] args ){
+        logger.info("Hello World");
+    }
+}
+
